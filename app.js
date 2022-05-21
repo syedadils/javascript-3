ebca38f4130d11d59f571a5660bc95f195add7c4
Syed ADil <101401945+syedadils@users.noreply.github.com>
2022-05-21 05:47:19 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+var age = prompt("Enter your age");
+// alert("I am " + age + " " + "years old");
+if(age >= 18){
+ alert("You are eligible to use this web");
+}
+if(age < 18){
+    alert("You are not eligible too use this web");
+}
+
+var visit = prompt("How many time you visit this site");
+alert("Today you visit this web" +" " + visit + " " + "time");
+
+// a visitor
+var visitorName = "John doe";
+console.log("visitor name is " + visitorName);
+alert("visitor name is " + visitorName)
+var item = "shirt";
+console.log("item :- " + item)
+alert("item :- " + item)
+var numbers = "5 T-shirt";
+console.log("Total Amount of shirt is :- " + numbers);
+alert("Total Amount of shirt is :- " + numbers)
+var store = "XYZ Clothing store";
+console.log("Store Name " + store);
+alert("Store Name " + store)
